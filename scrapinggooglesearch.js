e3085952a24e2f862d85eda159ccdc2e39a44d6a
Ronald Blanco <pnald.blanco@gmail.com>
2017-05-04 19:25:36 +0000

    scraping module

---
+++ scrapinggooglesearch.js
@@ -0,0 +1,57 @@
+//Module that scrap google search of photos
+
+module.exports = function (search,callback){
+    
+    var request = require('request');
+    var unhtml = require('unhtml');
+    var result = [];
+
+    function cleanCite(cite){
+        var result = [];
+        cite[0] = cite[0].split('">')[0];
+        for(var a = 0; a < cite.length; a++){
+            result.push(unhtml(cite[a].toString().replace('"','').replace('</cite>','').replace('&times;','x').replace('&nbsp;-&nbsp;',' - ').replace('&#8211;','-')));
+            //result.push(unhtml(cite[a]));
+        }
+        return result;
+    }
+
+    request('https://www.google.com/search?q='+ search +'&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiyocuqvNTTAhWGNSYKHUrjDiQQ_AUIBigB&biw=1910&bih=366', function (error, response, body) {
+            //console.log(response);
+            
+            if(error) throw error;
+            
+            var tbody = body.toString().split('<tbody')[1];
+            tbody = tbody.split("</tbody>")[0];
+            //var table = tbody.split('<table')[1];
+            var tr = tbody.split("<tr>"); //many tr
+        
+            for(var a = 2; a < tr.length; a++){
+                var td = tr[a].split("<td>");//begining, many 2
+                //console.log(td);
+            
+                var td2 = td[0].split("<td style=");//begining, 0
+                //console.log(td2);
+                
+                for(var b = 1; b < td2.length; b++){
+                    //refining process
+                    var aHref = td2[b].split("<a href=");
+                    aHref = aHref[1].split('</a><br>')[0];
+                    var img = aHref.split("<img height=")[1].split(' ')[1].replace('src="','').replace('"','');//ready
+                    var citeT = td2[b].split('<cite title=');
+                    citeT = citeT[1].split('</td>')[0].split('<br>');//the last 2, comment and dimensions and size, first is website
+                    var search = aHref.split('alt="')[1].replace('>','').replace('"',''); //ready
+                    //var title = td2[1].split('</cite><br>');
+                
+                    result.push({'img': img, 'search': unhtml(search), 'citeT': cleanCite(citeT)});
+                
+                }
+            }
+        
+        //console.log(result.length);
+        //console.log(result);
+        callback(error,result);
+    });
+
+    
+};
--- server.js
+++ server.js
@@ -5,129 +5,57 @@ var mongo = require('mongodb').MongoClient;
 var mongourl = "mongodb://short:short@ds129281.mlab.com:29281/";//remote
 var dbname = "urlshortener";
 var url = require('url');
-var request = require('request');
+//var request = require('request');
+var scrapinggooglesearch = require('./scrapinggooglesearch.js'); 
 var app = express();
 
-var newDoc = undefined;
-
-var randomIdGenerator = require('random-id-generator');
-//let defaultLengthId = randomIdGenerator();
-//defaultLengthId 3KDl9x 
-//default length is 6 
 
 
-/*function fCheckurl(url){
-    //var newDoc = undefined;
-    //checking url on database
-    //req.protocol + '://' + req.get('host') + '/checkurl/' + final
-    
-    request(url, function (error, response, body) {
-        //console.log('error:', error); // Print the error if one occurred 
-        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', JSON.parse(body).docs.length); // Print the HTML for the Google homepage. 
-        //var newDoc = undefined;
-        var checkurl = [];
-        if(error == null){
-            checkurl = JSON.parse(body);
-            if(checkurl.docs.length > 0) newDoc = true;
-            else if (checkurl.docs.length === 0) newDoc = false;
-            else newDoc = false;
-            console.log('newDoc in function->'+newDoc);
-        }
-        
-    });
-    //return newDoc;
-}*/
-
+//var newDoc = undefined;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/new/*', function (req, res) {
+app.get('/imagesearch/*', function (req, res) {
     
     //req info VAR
-    //var newDoc = undefined;
     var href = url.parse(req.url).href; //all GET
+    //imagesearch/lolcats%20funny?offset=10
     //var pahtname = url.parse(req.url).pahtname; //only path
-    //var query = url.parse(req.url).query; //only query
+    var criteria = url.parse(req.url).href.toString().split('?')[0].split('/')[2]; //only path
+    var query = url.parse(req.url).query; //only query ?
+    var cant_offset = parseInt(query.toString().split('=')[1]);
     //var method = req.method;
     //Loging Activity
     var mydate = new Date();
-    console.log(mydate+'-->SHORTENER CREATION!');
+    console.log(mydate+'-->IMAGE SEARCH!');
     console.log(href.toString());
-    console.log(href.toString().split('/')[2]+'//'+href.toString().split('/')[href.toString().split('/').length - 1]);
+    //console.log(pahtname);
+    console.log(criteria);
+    console.log(query);
+    console.log(cant_offset);
+    //console.log(href.toString().split('/')[2]+'//'+href.toString().split('/')[href.toString().split('/').length - 1]);
     //res.redirect(href.toString().split('/')[2]+'//'+href.toString().split('/')[4]);//OK READY
     
-    //Checking URL
-    var input = href.toString().split('/');
-    /*console.log('input->'+input);
-    console.log('input 2->'+input[2]);
-    console.log('input last->'+input[input.length - 1]);*/
-    var final = '';
-    var check = input[2]+'//'+input[input.length - 1];
-    //console.log('1->'+check);
-    check = check.match("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
-    //console.log('2->'+check);
-    if(check != null)final = check[0];
-    else final = check;
-    
-    //Preparing json
+    //Preparing json History of search
     var myJson;
     //myJson = JSON.stringify({
     myJson ={
-        'new_id': randomIdGenerator(3),// 3 dijits
-        'org_url': final
+        'search': criteria,// 3 dijits
+        'date': mydate
     };
     
-    //check if the url is new, New Doc
-    //fCheckurl(req.protocol + '://' + req.get('host') + '/checkurl/' + final);
-    request(req.protocol + '://' + req.get('host') + '/checkurl/' + final, function (error, response, body) {
-        //console.log('error:', error); // Print the error if one occurred 
-        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', JSON.parse(body).docs.length); // Print the HTML for the Google homepage. 
-        //var newDoc = undefined;
-        var checkurl = [];
-        if(error == null){
-            checkurl = JSON.parse(body);
-            if(checkurl.docs.length > 0) newDoc = false;
-            else if (checkurl.docs.length === 0) newDoc = true;
-            else newDoc = true;
-            console.log('newDoc in function->'+newDoc);
-        }
-        
-        //Connect and insert on database
-        if(check != null && newDoc === true){//if the url is valid and it is not on the database
-    console.log('Creating Doc');
-    mongo.connect(mongourl+dbname,function(err,db){
-        console.log('mongo connect');
-        if(err)throw err;
-        var collection = db.collection('shortener');
-        //var add = undefined;
-
-        //if(add === true && add != undefined){
-            collection.insert(myJson,function(err,data){
-                console.log('collection insert');
-                if(err)throw err;
-                console.log(data);
-                res.send({'shortened_URL':req.protocol + '://' + req.get('host') + '/' + myJson.new_id});
-                db.close;
-            });
-        //} 
-        
-    });
-    }else if(check != null && newDoc === false){
-        res.send({'shortened_URL':req.protocol + '://' + req.get('host') + '/' + checkurl.docs[0].new_id});
-    }else{//if the url is valid
-        res.send({'err':'URL not valid'});
+    function getModule(error,result){
+        console.log(error);
+        console.log({'result':result});
+        if(error === null) res.send({'result':result});
+        else res.send({'error':error});
     }
-        
-        
     
-    });
-    //console.log('newDoc after function->'+newDoc);
+    scrapinggooglesearch(criteria,getModule);//using my module
     
- 
+  //res.send();
 });
 
 app.get('/checkurl/*', function (req, res) {
@@ -237,7 +165,7 @@ app.get('*', function (req, res) {
 });
 
 app.listen(port, function () {
-  console.log('URL Shortener Api Server listening on port '+port+'!');
+  console.log('Image Search A Layer Api Server listening on port '+port+'!');
 });
 
 
